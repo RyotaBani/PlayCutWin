1df4e568244f76b588f56276a7fae57e9951d27f
Ryota Bani <ryota.1006.bsk.35@gmail.com>
2026-01-20 16:14:33 +0900

    Fix layout ratio + MediaEnded + VideoHint

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Threading;
 
 namespace PlayCutWin
