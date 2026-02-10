008439f1f2369a4e90f9fedfe1fb874f9ef2c071
Ryota Bani <ryota.1006.bsk.35@gmail.com>
2026-02-11 07:55:25 +0900

    fix: remove duplicate textbox handlers (keep only MainWindow.xaml.cs)

--- TextBoxHandlers.cs
+++ TextBoxHandlers.cs
@@ -1,27 +1,14 @@
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
+// NOTE:
+// MainWindow.xaml.cs already contains the following handlers:
+// - TextBox_SelectAllOnFocus
+// - TextBox_PreviewMouseLeftButtonDown_SelectAll
+//
+// This file is intentionally left empty to avoid duplicate member definitions.
 
 namespace PlayCutWin
 {
     public partial class MainWindow
     {
-        private void TextBox_SelectAllOnFocus(object sender, RoutedEventArgs e)
-        {
-            if (sender is TextBox tb)
-            {
-                tb.SelectAll();
-            }
-        }
-
-        private void TextBox_PreviewMouseLeftButtonDown_SelectAll(object sender, MouseButtonEventArgs e)
-        {
-            if (sender is TextBox tb && !tb.IsKeyboardFocusWithin)
-            {
-                e.Handled = true;
-                tb.Focus();
-                tb.SelectAll();
-            }
-        }
+        // intentionally empty
     }
 }
